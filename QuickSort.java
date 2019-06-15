007401ab90512a501d86552b3fd43e2c74d4a5c1
Alex Leibowitz <alex.leibowitz@zoro.com>
2019-06-15 15:55:06 -0500

    finished working through implementation of quicksort (with help from book :)

--- QuickSort.java
+++ QuickSort.java
@@ -8,37 +8,51 @@ import java.util.Arrays;
 
 public class QuickSort {
 
-    public Integer[] sort(Integer[] a) {
-        int pivot = a.length / 2;
-        partition(a, 0, pivot, a.length - 1);
-        return a;
+    public static void sort(int[] a, int lo, int hi) {
+        if (hi <= lo) return;
+        int j = partition(a, lo, hi);
+        // the left sub-array
+        sort(a, lo, j-1);
+        // the right sub-array
+        sort(a, j+1, hi);
     }
 
-    void partition(Integer[] a, int lo, int pivot, int hi) {
-        if (lo >= hi) return;
-        int pivotElement = a[pivot];
-        while (lo < hi) {
-            if (lo == pivot) lo++;
-            if (hi == pivot) hi--;
-            while (a[lo++] < pivotElement) if (lo == a.length - 1) break;
-            while (a[hi--] > pivotElement) if (hi == 0) break;
-            swap(a, lo, hi);
+    static int partition(int[] a, int lo, int hi) {
+        int l = lo;
+        // we have to do this -- if we tried "h--" below and set h = hi, won't work
+        // this is necessary because we want to exchange the first element, not skip past it.
+        int h = hi + 1;
+        int pivot = a[lo];
+        while (true) {
+            // l=lo is <= itself, so start with the next
+            while (a[++l] <= pivot) if (l == hi) break;
+            while (a[--h] >= pivot) if (h == lo) break;
+            // we should break when everything to the left is <= and everything to the right is >=
+            // what happens when they cross?  If left and right have crossed cross...
+            // that means that the first item > pivot on the left side is after the first item < pivot on the left
+            // or another way: everything to the left of l is < pivot, everything to the right of r is >
+            // when they cross, we don't want to exchange or we break the invariant
+            if (l >= h) break;
+            swap(a, l, h);
         }
-        swap(a, pivot, lo);
-        int quarter = (lo + hi ) / 4;
-        partition(a, lo + quarter, lo + (lo + 2 * quarter) / 2, lo + 2 * quarter);
-        partition(a, hi - 2 * quarter, hi - (hi - 2 * quarter) / 2, hi);
+        // pick a new pivot ????
+        // currently a[h] should be < pivot, so when we swap, everything to the left of the pivot is less
+        // and everything to right will be >=
+        swap(a, lo, h);
+        // return the new location of the pivot -- the array will be divided into two new "arrays" -- the subsection
+        // to the left of the pivot and the subsection to the right.
+        return h;
     }
 
-    void swap(Integer[] a, int x, int y) {
+    static void swap(int[] a, int x, int y) {
         int temp = a[x];
         a[x] = a[y];
         a[y] = temp;
     }
 
     public static void main(String[] args) {
-        StdOut.println("Hello world!");
-        QuickSort q = new QuickSort();
-        StdOut.println(Arrays.deepToString(q.sort(new Integer[] {4, 3, 2, 1})));
+        int[] toSort = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+        sort(toSort, 0, toSort.length - 1);
+        StdOut.println(toSort);
     }
 }
